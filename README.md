26890903bb25fa98140d51e79d311196bfbc9156
bcko <gobcko@gmail.com>
2017-10-28 23:29:37 -0400

    location object

--- README.md
+++ README.md
@@ -4,25 +4,18 @@
 ### API
 
 [Google Maps APIs](https://developers.google.com/maps/)
-
 [Google Maps Javascript API](https://developers.google.com/maps/documentation/javascript/)
 
-[SODA Developers](https://dev.socrata.com)
 
 ### Javascript
 
 [KnockoutJS](http://knockoutjs.com/)
 
+[MDN : strict mode](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode)
+[MDN : Object.freeze](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/freeze)
+
 ### CSS
 
 [Material Components for the Web](https://material.io/components/web/)
-
-### Data
-[NYC Open Data](https://opendata.cityofnewyork.us/)
-
-[NYC Department of City Planning Open Data](https://www1.nyc.gov/site/planning/data-maps/open-data.page)
-
-[New York State's Developer Resources Page](https://data.ny.gov/developers)
-
 
 
--- js/app.js
+++ js/app.js
@@ -1,4 +1,18 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
+/*
+First, strict mode eliminates some JavaScript silent errors by changing them to throw errors. 
+Second, strict mode fixes mistakes that make it difficult for JavaScript engines to perform optimizations: strict mode code can sometimes be made to run faster than identical code that's not strict mode. 
+Third, strict mode prohibits some syntax likely to be defined in future versions of ECMAScript.
+*/
 "use strict";
+
+const locations = [
+{}
+
+] 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/freeze
+// prevents new properties from being added to it; prevents existing properties from being removed; and prevents existing properties, or their enumerability, configurability, or writability, from being changed
+Object.freeze(locations)
 
 let map;
 function initMap() {

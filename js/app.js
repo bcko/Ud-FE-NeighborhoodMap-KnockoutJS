61d3d4247e5c1f14a6209493f74de8ee750bb23b
bcko <gobcko@gmail.com>
2017-10-29 21:10:08 -0400

    use strict inside functions

--- js/app.js
+++ js/app.js
@@ -1,12 +1,3 @@
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode
-/*
-First, strict mode eliminates some JavaScript silent errors by changing them to throw errors. 
-Second, strict mode fixes mistakes that make it difficult for JavaScript engines to perform optimizations: strict mode code can sometimes be made to run faster than identical code that's not strict mode. 
-Third, strict mode prohibits some syntax likely to be defined in future versions of ECMAScript.
-*/
-/*jshint strict:false */
-"use strict";
-
 
 // location lattitude and longitude retrieved from google maps (don't forget to visit these restaurants if you ever visit Ann Arbor)
 const restaurants = [
@@ -44,6 +35,7 @@ Object.freeze(restaurants);
 let google;
 // https://developers.google.com/maps/documentation/javascript/tutorial
 function initMap() {
+	"use strict";
 	let map = new google.maps.Map(document.getElementById('map'), {
 	  center: {lat: 42.2806678, lng: -83.7376554},
 	  zoom: 16,
@@ -56,7 +48,7 @@ function initMap() {
 
 // Add markers to the map
 function initMarkers(map) {
-	
+	"use strict";
 	// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of
 	// The for...of statement creates a loop iterating over iterable objects (including Array, Map, Set, String, TypedArray, arguments object and so on), invoking a custom iteration hook with statements to be executed for the value of each distinct property
 	for (const restaurant of restaurants) {
